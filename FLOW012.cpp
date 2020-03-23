301d4cbf2a833c2c794ac8edd62d069eff954fb6
bhoomika-pandey <47912222+bhoomika-pandey@users.noreply.github.com>
2020-03-23 12:22:56 +0530

    Add files via upload

---
+++ FLOW012.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+    cin>>t;
+	while(t--)
+	{
+        int a,b,c,sum;
+	    cin>>a>>b>>c;
+        sum=a+b+c;
+        if(sum<=180)
+        {
+           cout<<"YES"<<endl;
+        }
+        else
+        {
+           cout<<"NO"<<endl; 
+        }
+        	
+        }
+	return 0;
+}
